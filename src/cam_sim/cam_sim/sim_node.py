924cf8667cd7652bf57cc96ae7b9b80d30bc2605
pranav-trip <pranavtr@bu.edu>
2025-07-03 09:58:26 -0400

    added depth in sim

--- src/cam_sim/cam_sim/sim_node.py
+++ src/cam_sim/cam_sim/sim_node.py
@@ -11,55 +11,84 @@ class camSim(Node):
     def __init__(self):
         super().__init__('cam_sim')
     
-        self.x_lim, self.y_lim = 80, 60
+        self.x_lim, self.y_lim, self.z_lim = 80, 60, 60
 
-        self.drone_x , self.drone_y = self.x_lim/2, self.y_lim/2
-        self.drone_dx, self.drone_dy = 0, 0
+        self.drone_x , self.drone_y, self.drone_z = self.x_lim/2, self.y_lim/2, self.z_lim/5
+        self.drone_dx, self.drone_dy, self.drone_dz = 0, 0, 0
 
-        self.world_points = [(random.uniform(0, self.x_lim), random.uniform(0, self.y_lim)) for x in range(10)]
+        self.world_points = [
+            (random.uniform(0, self.x_lim), 
+             random.uniform(0, self.y_lim),
+             random.uniform(0, self.z_lim)) 
+             for x in range(10)]
 
-        self.timer = self.create_timer(0.005, self.timer_callback)
+        self.focal_length = self.z_lim
+        self.timer = self.create_timer(0.005, self.update)
         self.last_printed = None
     
     def key_control(self, event):
         step = 0.2
         if event.key == 'right': self.drone_dx = min(self.drone_dx + step, self.x_lim/2)
         elif event.key == 'left': self.drone_dx = max(self.drone_dx - step, -self.x_lim/2)
-        elif event.key == 'up': self.drone_dy = min(self.drone_dy + step, self.y_lim/2)
+        
+        if event.key == 'up': self.drone_dy = min(self.drone_dy + step, self.y_lim/2)
         elif event.key == 'down': self.drone_dy = max(self.drone_dy - step, -self.y_lim/2)
+        
+        if event.key == "i":  self.drone_dz = min(self.drone_dz + step, self.z_lim / 2)
+        elif event.key == "o":  self.drone_dz = max(self.drone_dz - step, -self.z_lim / 2)
 
-    def timer_callback(self):
+    def update (self):
         plt.clf()
 
-        self.transformed_points = [self.transform(x, y) for x, y in self.world_points]
+        self.transformed_points = [self.transform(x, y, z) for x, y, z in self.world_points]
         
         if self.last_printed != self.transformed_points:
             os.system("clear")
-            self.get_logger().info(f'Drone Position: x = {self.drone_x+self.drone_dx:.2f}, y = {self.drone_y+self.drone_dy:.2f}\n')
+            self.get_logger().info(f'Drone Position: x = {self.drone_x+self.drone_dx:.2f}, y = {self.drone_y+self.drone_dy:.2f}, z = {self.drone_z+self.drone_dz:.2f}\n')
 
-            for i, (x, y) in enumerate(self.transformed_points):
-                x_org, y_org = self.world_points[i]
-                self.get_logger().info(f'Point {i}: x = {x_org:.2f}, y = {y_org:.2f}')
-                self.get_logger().info(f'Drone Frame: x = {x:.2f}, y = {y:.2f}\n')
+            for i, (x, y, z) in enumerate(self.transformed_points):
+                x_org, y_org, z_org = self.world_points[i]
+                self.get_logger().info(f'Point {i}: x = {x_org:.2f}, y = {y_org:.2f}, z = {z_org:.2f}')
+                self.get_logger().info(f'Drone Frame: x = {x:.2f}, y = {y:.2f}, z = {z:.2f}\n')
             
             self.last_printed = self.transformed_points
 
-        for i, (x, y) in enumerate(self.transformed_points):    
+        for i, (x, y, z) in enumerate(self.transformed_points):    
             plt.plot(x, y, 'bo', markersize=12)
             plt.plot(x, y, marker=f'${i}$', markersize=7, color='white') 
 
-        
-        plt.plot(self.drone_x, self.drone_y, 'ko', markersize=12)
-        plt.xlim(self.x_lim*0.25, self.x_lim*0.75) 
-        plt.ylim(self.y_lim*0.25, self.y_lim*0.75)
+        self.drone_x += self.drone_dx
+        self.drone_y += self.drone_dy
+        self.drone_z += self.drone_dz
+
+        self.drone_dx = 0
+        self.drone_dy = 0
+        self.drone_dz = 0
+
+        plt.plot(0, 0, 'ko', markersize=12)
+        plt.xlim(-self.focal_length, self.focal_length)
+        plt.ylim(-self.focal_length, self.focal_length)
+
 
         plt.axis("off")
         plt.draw()
         plt.pause(0.005)
 
-    def transform(self, x, y):
-        return x-self.drone_dx, y-self.drone_dy
+    def transform(self, x, y, z):
+        dx = x - self.drone_x
+        dy = y - self.drone_y
+        dz = z - self.drone_z
+
+        if dz <= 0:
+           return float('inf'), float('inf'), dz
+
+        x_proj = self.focal_length * (dx / dz)
+        y_proj = self.focal_length * (dy / dz)
+        return x_proj, y_proj, dz
     
+        
+
+        
 def main(args=None):
     rclpy.init(args=args)
     node = camSim()
