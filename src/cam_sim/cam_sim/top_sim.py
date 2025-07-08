f7369aa53dbe347490bd1377766b068d6ee9c36c
pranav-trip <pranavtr@bu.edu>
2025-07-08 16:29:21 -0400

    error correction using weighted side velocities

--- src/cam_sim/cam_sim/top_sim.py
+++ src/cam_sim/cam_sim/top_sim.py
@@ -19,37 +19,40 @@ class topSim(Node):
         self.focal_length = 60
 
         self.drone_x, self.drone_y, self.drone_z = 0.0, 0.0, -self.height/2
-        self.drone_dx, self.drone_dy, self.drone_dz = 0.0, 0.0, 0.0
+        self.drone_dx, self.drone_dy, self.drone_dz = 0.0, 0.0, 1.0
 
-        self.init_frames = 50
         self.frame_count = 0
 
         self.points = self.generate_points()
+        self.transformed_points = []
         self.timer = self.create_timer(0.02, self.update)
 
     def generate_points(self):
         points = []
+        point_count = 5
 
-        for i in range(5):
+        for i in range(point_count):
             x = -self.bound_width
-            y = random.uniform(-self.depth/2, self.depth/2)
-            z = random.uniform(-self.height/2, 0)
+            if i%2 == 0: y = random.uniform(-self.height/2, 0)
+            else: y = random.uniform(0, self.height/2)
+            z = random.uniform(-self.height/2+20, self.height/2)
             points.append({'num': i, 'x': x, 'y': y, 'z': z})
 
-        for i in range(5):
+        for i in range(point_count):
             x = self.bound_width
-            y = random.uniform(-self.depth/2, self.depth/2)
-            z = random.uniform(-self.height/2, 0)
-            points.append({'num': i + 5, 'x': x, 'y': y, 'z': z})
+            if i%2 == 0: y = random.uniform(-self.height/2, 0)
+            else: y = random.uniform(0, self.height/2)
+            z = random.uniform(-self.height/2+20, self.height/2)
+            points.append({'num': i + point_count, 'x': x, 'y': y, 'z': z})
 
         return points
-
+    
     def plot(self):
         for side in [-1, 1]:
             x = side * self.bound_width
             plt.plot([x, x], [-self.height/2, self.height/2], color = 'black', linestyle = '-')
 
-        if abs(self.drone_x) < 0.01 and abs(self.drone_y) < 0.01: clr = 'green'
+        if abs(self.drone_x) < 1 and abs(self.drone_y) < 1: clr = 'green'
         else: clr = 'red'
 
         plt.plot([0, 0], [-self.height/2, self.height/2], color = clr, linestyle = '--')
@@ -60,20 +63,15 @@ class topSim(Node):
 
         plt.plot(self.drone_x, self.drone_z, 'ro', markersize=12)
 
-    def update(self):
-        plt.clf()
+    def control(self):
+        left_vx_sum, right_vx_sum = 0.0, 0.0
+        left_vx_avg, right_vx_avg = 0.0, 0.0
 
-        if self.frame_count < self.init_frames:
-            self.drone_dz = 1.0
-            self.frame_count += 1
-            return
-        
-        elif self.drone_z == self.height/2: 
-            return
+        bottom_vy_sum, top_vy_sum = 0.0, 0.0
+        bottom_vy_avg, top_vy_avg = 0.0, 0.0
 
-        sum_vx, sum_vy = 0.0, 0.0
+        total_weight = 0.0
         weights = []
-
 
         for point in self.points:
             dx = point['x'] - self.drone_x
@@ -87,23 +85,37 @@ class topSim(Node):
             Vy = (-self.drone_dy * dz + self.drone_dz * dy) / (dz ** 2)
 
             speed = (Vx**2 + Vy**2)**0.5
-            weight = 1.0/max(speed, 0.001)
-            weights.append((weight, Vx, Vy))
+            weight = 1.0 / max(speed, 0.001)
 
-        total_weight = sum(weight for weight, Vx, Vy in weights)
-        if total_weight == 0: total_weight = 1.0
+            weights.append((dx, dy, weight, Vx, Vy))
+            total_weight += weight
 
-        avg_vx = sum(weight * Vx for weight, Vx, Vy in weights)/total_weight
-        avg_vy = sum(weight * Vy for weight, Vx, Vy in weights)/total_weight
+        if total_weight == 0:
+            total_weight = 1.0
 
-        sum_vx, sum_vy = 0.0, 0.0
-        for weight, Vx, Vy in weights:
-            norm_weight = weight / total_weight
-            sum_vx += norm_weight * (Vx - avg_vx)
-            sum_vy += norm_weight * (Vy - avg_vy)
+        for dx, dy, weight, Vx, Vy in weights:
+            if dx < 0: left_vx_avg += weight * Vx
+            else: right_vx_avg += weight * Vx
 
-        self.drone_dx = sum_vx
-        self.drone_dy = sum_vy
+            if dy < 0: bottom_vy_avg += weight*Vy
+            else: top_vy_avg += weight*Vy
+
+        left_vx_avg /= total_weight
+        right_vx_avg /= total_weight
+        bottom_vy_avg /= total_weight
+        top_vy_avg /= total_weight
+
+        for dx, dy, weight, Vx, Vy in weights:
+            norm_weight = weight/total_weight
+
+            if dx < 0: left_vx_sum += norm_weight * (Vx - left_vx_avg)
+            else: right_vx_sum += norm_weight * (Vx - right_vx_avg)
+
+            if dy < 0: bottom_vy_sum += norm_weight * (Vy - bottom_vy_avg)
+            else: top_vy_sum += norm_weight * (Vy - top_vy_avg)
+
+        self.drone_dx = left_vx_sum + right_vx_sum
+        self.drone_dy = bottom_vy_sum + top_vy_sum
         self.drone_dz = 1.0
 
         self.drone_x += self.drone_dx
@@ -111,10 +123,8 @@ class topSim(Node):
         self.drone_z += self.drone_dz
         self.frame_count += 1
 
-        for point in self.points:
-            if point['z'] < self.drone_z: point['z'] += random.uniform(0, self.height/2)
-
-        transformed, velocities = [], []
+    def transform(self):
+        self.transformed_points = []
 
         for point in self.points:
             dx = point['x'] - self.drone_x
@@ -130,17 +140,36 @@ class topSim(Node):
             Vx = (-self.drone_dx * dz + self.drone_dz * dx) / (dz ** 2)
             Vy = (-self.drone_dy * dz + self.drone_dz * dy) / (dz ** 2)
 
-            transformed.append({'num': point['num'], 'x': x_proj, 'y': y_proj, 'world_x': point['x'], 'world_y': point['y'], 'world_z': point['z']})
-            velocities.append((Vx, Vy))
+            self.transformed_points.append({'num': point['num'], 
+                'x': x_proj, 'y': y_proj, 
+                'world_x': point['x'], 'world_y': point['y'], 'world_z': point['z'], 
+                'Vx': Vx, 'Vy': Vy})
+
+    def update(self):
+        plt.clf()
+        
+        if self.drone_z == self.height/2: 
+            return
+        
+        if (self.frame_count == 80):
+            self.drone_x = 5
+
+        for point in self.points:
+            if point['z'] < self.drone_z + 20: 
+                point['z'] += random.uniform(self.drone_z + 20, self.height/2)
+                if point['y'] < 0: point['y'] = random.uniform(-self.height/2, 0)
+                else: point['y'] = random.uniform(0, self.height/2)
+
+        self.control()
+        self.transform()
 
         os.system("clear")
-        self.get_logger().info(f"Drone Position: x = {self.drone_x:.2f}, y = {self.drone_y:.2f}, z = {self.drone_z:.2f}\n")
+        print(f"Drone Position: x = {self.drone_x:.2f}, y = {self.drone_y:.2f}, z = {self.drone_z:.2f}\n")
 
-        for i, point in enumerate(transformed):
-            Vx, Vy = velocities[i]
-            self.get_logger().info(f"Point {point['num']}: x = {point['world_x']:.2f}, y = {point['world_y']:.2f}, z = {point['world_z']:.2f}")
-            self.get_logger().info(f"Drone Frame: x = {point['x']:.2f}, y = {point['y']:.2f}")
-            self.get_logger().info(f"Velocity: Vx = {Vx:.2f}, Vy = {Vy:.2f}\n")
+        for i, point in enumerate(self.transformed_points):
+            print(f"Point {point['num']}: x = {point['world_x']:.2f}, y = {point['world_y']:.2f}, z = {point['world_z']:.2f}")
+            print(f"Drone Frame: x = {point['x']:.2f}, y = {point['y']:.2f}")
+            print(f"Velocity: Vx = {point['Vx']:.2f}, Vy = {point['Vy']:.2f}\n")
         
         self.plot()
 
