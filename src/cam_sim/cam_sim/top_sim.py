fd012b21cf2b8b4ca468318143957d95469a8c4b
pranav-trip <pranavtr@bu.edu>
2025-07-09 16:41:59 -0400

    added scaling on top/bottom control

--- src/cam_sim/cam_sim/top_sim.py
+++ src/cam_sim/cam_sim/top_sim.py
@@ -84,6 +84,9 @@ class topSim(Node):
 
         print(f"Left Point Vx: {Vx_left * 1000:.2f}, Right Point Vx: {Vx_right * 1000:.2f}")
 
+    def scale(self, velocity):
+        return 1000 * abs(velocity*10000)**2
+
     def control(self):
         left_vx_avg, right_vx_avg = 0.0, 0.0
         bottom_vy_avg, top_vy_avg = 0.0, 0.0
@@ -125,10 +128,8 @@ class topSim(Node):
 
         self.sample_points()
 
-        left_scale = 1000 * abs(left_vx_avg*10000)**2
-        right_scale = 1000 * abs(right_vx_avg*10000)**2
-
-        self.drone_dx = ((left_vx_avg * left_scale) + (right_vx_avg * right_scale))
+        self.drone_dx = (left_vx_avg * self.scale(left_vx_avg)) + (right_vx_avg * self.scale(left_vx_avg))
+        self.drone_dy = (bottom_vy_avg * self.scale(bottom_vy_avg)) + (top_vy_avg * self.scale(top_vy_avg))
         self.drone_dz = 1.0
 
         plt.plot(80, 0, 'ko', markersize=8)
