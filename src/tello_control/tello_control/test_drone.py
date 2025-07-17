038d626a7165f132018f323569cadde1c5f13c3d
pranav-trip <pranavtr@bu.edu>
2025-07-17 11:23:13 -0400

    restored version

---
+++ src/tello_control/tello_control/test_drone.py
@@ -0,0 +1,31 @@
+import rclpy
+from rclpy.node import Node
+from djitellopy import Tello
+import time
+
+class testTello(Node):
+    def __init__(self):
+        super().__init__('testTello')
+        self.tello = Tello()
+        self.tello.connect()
+        print(f"Battery: %d%%" % self.tello.get_battery())
+
+        self.timer = self.create_timer(2.0, self.test)
+
+    def test(self):
+        self.timer.cancel()
+        self.tello.takeoff()
+
+        self.tello.send_rc_control(0, 40, 0, 0)
+        time.sleep(2)
+        self.tello.send_rc_control(0, 0, 0, 0)
+
+        self.tello.land()
+        self.tello.end()
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = testTello()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
